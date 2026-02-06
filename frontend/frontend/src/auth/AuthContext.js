bfd454124b9e0bc1c6ec31e97f53626042befe99
Parusa123 <parthapratimmahanta3@gmail.com>
2026-02-06 21:57:34 +0530

    react is running

---
+++ frontend/frontend/src/auth/AuthContext.js
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+import api from "../api/axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = async (email, password) => {
+    const res = await api.post("/auth/login", { email, password });
+
+    localStorage.setItem("token", res.data.token);
+    setUser(res.data.user);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
---
+++ frontend/frontend/src/pages/Login.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useAuth } from "../auth/AuthContext";
+
+export default function Login() {
+  const { login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      alert("Login successful ✅");
+    } catch (err) {
+      alert("Login failed ❌");
+    }
+  };
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /><br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        /><br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
